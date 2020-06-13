6bc01bbbafa17d884d4193710e248ceb73f09879
Adam Vandolder <adam.vandolder@gmail.com>
2020-06-12 21:38:58 -0400

    Add inital Pest grammar and example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 /**
+ * # Pattern Match
+ *
  * Handle pattern matching for a small Erlang-esque system.
  * Has first-class tuples, records, arrays, ranges, ints, strings, chars, bools,
  * and symbols (:id atomic strings).
  *
- * A rough syntax mockup:
+ * ## Syntax Mockup
  *
  * match val with
  *   (x, y) => x + y
@@ -15,24 +17,50 @@
  *   'A' | 'B' => "AB"
  *   [x, _, z] => x + z
  *   _ => "default"
- *   Int(x) => $"{x} is an int"
+ *   is Int => $"{val} is an int"
  * end
  *
- * Grammar:
- * match -> 'match' value 'with' cases
+ * ## Grammar
+ *
+ * match -> 'match' expr 'with' cases
  * cases -> pats '=>' expr cases | 'end'
  * pats -> pat '|' pats | pat
  * pat -> '_' | id | int | string | char | bool | symbol | tuple_pat | record_pat | array_pat
- *      | range | type_pat
+ *      | range
  * tuple_pat -> '(' elems
  * array_pat -> '[' array_elems
  * record_pat -> '{' fields
- * type_pat -> type_name '(' pat ')'
  * elems -> pat ',' elems | pat ')' | ')'
  * array_elems -> pat ',' array_elems | pat ']' | '...' id ']' | '...' ']' | ']'
  * fields -> field ',' fields | field '}' | '...' id '}' | '...' '}'
  * field -> '[' expr ']' ':' pat | id ':' pat | id
  */
 
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct PatternParser;
+
 fn main() {
+    let example = r#"match val with
+  (x, y) => x
+  [1, 2, ...rest] => rest
+  {tag: :pat, val, line: l} => val
+  "hello" => "world"
+  1..10 => :small
+  'a'..'z' => lower
+  'A' | 'B' => "AB"
+  [_, _, z] => z
+  _ => "default"
+end"#;
+
+    let program = PatternParser::parse(Rule::Program, example)
+        .expect("failed parse")
+        .next()
+        .unwrap();
+    print!("{:#?}", program);
 }
